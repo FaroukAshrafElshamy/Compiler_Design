2d3e092e04fd226d75943cd3efee6db51f340264
devkalid <khalod7.2015@gmail.com>
2024-11-26 22:13:34 +0200

    v3.2

--- compiler/tekonization_phase.py
+++ compiler/tekonization_phase.py
@@ -41,7 +41,7 @@ def read_file(file_path):
 # Main program
 if __name__ == '__main__':
     # Specify the file containing the code
-    file_path = 'code.txt'
+    file_path = 'conistants/code.txt'
 
     # Read code from the file
     code = read_file(file_path)
