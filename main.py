3663ca244deb2658dc6fd7bd768334c43dab0974
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-12-02 21:27:36 +0200

    Main Interface

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from PySide6.QtWidgets import QApplication,QMainWindow, QMenu, QWidget, QWidgetAction
+from PySide6.QtWidgets import QApplication,QMainWindow, QWeb
+# from Custom_Widgets.Widgets import *
 from PySide6.QtCore import Qt
 from ui_index3 import Ui_MainWindow
 import sys
@@ -8,6 +9,36 @@ class MySideBar(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.setWindowTitle("CodeX")
+        # loadJsonStyle(self, self.ui)
+
+    # Connect Buttons
+        self.ExplainButton.clicked.connect(self.S_Explain)
+        self.EditorButton.clicked.connect(self.S_Editor)
+        self.OutputButton.clicked.connect(self.S_Output)
+        self.HubButton.clicked.connect(self.S_Hub)
+        self.AboutButton.clicked.connect(self.S_About)
+
+    # Add pdf
+        # self.
+
+    # Methods to switch to different pages
+    def S_Explain(self):
+        self.Main_page.setCurrentIndex(0)
+
+    def S_Editor(self):
+        self.Main_page.setCurrentIndex(1)
+
+    def S_Output(self):
+        self.Main_page.setCurrentIndex(2)
+
+    def S_Hub(self):
+        self.Main_page.setCurrentIndex(3)
+
+    def S_About(self):
+        self.Main_page.setCurrentIndex(4)
+
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
