2e83b646741e5f27df42f7baccd583fd7be45c87
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-12-04 00:14:54 +0200

    Symbol table after linking with GUI

--- compiler/symbol_table.py
+++ compiler/symbol_table.py
@@ -1,72 +1,61 @@
 
-from tokenization_phase import main
+from .tokenization_phase import main
 import pandas as pd
 
-def get_the_identifiers_only():
-    file_path = 'constants/code.txt'
-    code, tokens = main(file_path, 2)
-    
-    identifiers = []
-    for identifier in tokens:
-        if identifier[0] == "IDENTIFIER":
-            if identifier[1] not in identifiers:
-                identifiers.append(identifier[1])
-    
-    return identifiers
+def symbolTalbe(file_path):
+
+    def get_the_identifiers_only():
+        code, tokens = main(file_path, 2)
+        identifiers = []
+        for identifier in tokens:
+            if identifier[0] == "IDENTIFIER":
+                if identifier[1] not in identifiers:
+                    identifiers.append(identifier[1])
+        return identifiers
 
 
-def numbers_file():
-    lines = []
-    with open('constants/code.txt', "r") as file:
-            for line_content in file:
-                lines.append(line_content.strip())
-    return lines
-
-def get_line_appears(iden):
-    lines = numbers_file()
-    lines_for_IDEN = []
-    for each in lines:
-        if iden in each:
-            lines_for_IDEN.append(lines.index(each)+1)
-    line_declared = lines_for_IDEN[0]
-    lines_of_usage = lines_for_IDEN[1:]
-    return line_declared , lines_of_usage
-
-
-
-def show_the_code_indexed():
-    with open('constants/code.txt', "r") as file:
-            for line_number, line_content in enumerate(file, start=1):
-                print(f"Line {line_number}: {line_content.strip()}")
+    def numbers_file():
+        lines = []
+        with open(file_path, "r") as file:
+                for line_content in file:
+                    lines.append(line_content.strip())
+        return lines
+    def get_line_appears(iden):
+        lines = numbers_file()
+        lines_for_IDEN = []
+        for each in lines:
+            if iden in each:
+                lines_for_IDEN.append(lines.index(each)+1)
+        line_declared = lines_for_IDEN[0]
+        lines_of_usage = lines_for_IDEN[1:]
+        return line_declared , lines_of_usage
 
 
 
-class SymbolTalbe:
-    def __init__(self, name, type, size, dim, lineDeclared, linesOfUsage) -> None:
-        self.name = name
-        self.type = type
-        self.size = size
-        self.dim = dim
-        self.lineDeclared = lineDeclared
-        self.lineOfUsage = linesOfUsage
+    def show_the_code_indexed():
+        with open(file_path, "r") as file:
+                for line_number, line_content in enumerate(file, start=1):
+                    print(f"Line {line_number}: {line_content.strip()}")
 
 
+    class SymbolTalbe:
+        def __init__(self, name, type, size, dim, lineDeclared, linesOfUsage) -> None:
+            self.name = name
+            self.type = type
+            self.size = size
+            self.dim = dim
+            self.lineDeclared = lineDeclared
+            self.lineOfUsage = linesOfUsage
+
+    symbols = []
+
+    for iden in get_the_identifiers_only():
+        symbols.append(SymbolTalbe(iden,"null","null",0,get_line_appears(iden)[0],get_line_appears(iden)[1]))
+
+    data = [vars(symbol) for symbol in symbols]
+    df = pd.DataFrame(data)
+    print(df)
 
 
-
-symbols = []
-
-for iden in get_the_identifiers_only():
-    symbols.append(SymbolTalbe(iden,"null","null",0,get_line_appears(iden)[0],get_line_appears(iden)[1]))
-
-
-
-data = [vars(symbol) for symbol in symbols]
-df = pd.DataFrame(data)
-print(df)
-
-
-
-
-# save on the csv file
-df.to_csv('compiler/Output/symbol_table.csv', index=False)
+    # save on the csv file
+    df.to_csv('compiler/Output/symbol_table.csv', index=False)
