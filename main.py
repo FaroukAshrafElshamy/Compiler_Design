06fca12c6a72f967322f1bbcc7cf180d7e45c663
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-12-03 23:03:19 +0200

    Adding The Output

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog
+from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog, QTableWidgetItem
 from ui_index3 import Ui_MainWindow
 from PySide6.QtGui import QIcon
 from compiler.tokenization_phase import main
+from compiler.First import First
+import csv
 import sys
 
 class MySideBar(QMainWindow, Ui_MainWindow):
@@ -10,7 +12,7 @@ class MySideBar(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.setWindowTitle("CodeX")
         self.setWindowIcon(QIcon('resources/compiler(1).png'))
-        self.TabSize(5) # Tab Size
+        self.TabSize(7) # Tab Size
 
     # Connect Buttons
         self.ExplainButton.clicked.connect(self.S_Explain)
@@ -21,7 +23,9 @@ class MySideBar(QMainWindow, Ui_MainWindow):
         self.ClearButton.clicked.connect(self.S_Clear)
         self.Open_file.clicked.connect(self.S_OpenFile)
         self.Save_file.clicked.connect(self.S_SaveFile)
-        self.RunButton.clicked.connect(self.append_file)
+        self.RunButton.clicked.connect(self.OutputTokens)
+        self.RunButton.clicked.connect(self.OutputFirst)
+        self.RunButton.clicked.connect(self.OutputSymboleTable)
         
 
     # Methods to switch to different pages
@@ -43,27 +47,29 @@ class MySideBar(QMainWindow, Ui_MainWindow):
     def S_Clear(self):
         self.textEdit.clear()
         self.TextOutput.clear()
+        self.fileName.clear()
 
     def S_OpenFile(self):
         global file_path
         file_path, _ = QFileDialog.getOpenFileName(self, "Open Text File", "", "Text Files (*.txt);;All Files (*)")
+        main(file_path, 1)
         if file_path:
             try:
                 with open(file_path, "r", encoding="utf-8") as file:
                     content = file.read()
-                self.textEdit.setPlainText(content)  # Display the content in QTextEdit
+                self.textEdit.setPlainText(content)
                 self.fileName.setText(file_path)
             except Exception as e:
                 self.textEdit.setPlainText(f"Failed to load the file.\nError: {e}")
 
     def S_SaveFile(self):
-        # Open a file dialog for saving the file
-        file_path, _ = QFileDialog.getSaveFileName(self, "Save Text File", "", "Text Files (*.txt);;All Files (*)")
-        if file_path:  # If a file path is provided
+        file_path2, _ = QFileDialog.getSaveFileName(self, "Save Text File", "", "Text Files (*.txt);;All Files (*)")
+        if file_path2:
             try:
-                with open(file_path, "w", encoding="utf-8") as file:
-                    content = self.textEdit.toPlainText()  # Get content from QTextEdit
-                    file.write(content)  # Write content to the file
+                with open(file_path2, "w", encoding="utf-8") as file:
+                    content = self.textEdit.toPlainText()
+                    file.write(content)
+                main(file_path2, 1)
             except Exception as e:
                 self.text_edit.setPlainText(f"Failed to save the file.\nError: {e}")
 
@@ -72,18 +78,45 @@ class MySideBar(QMainWindow, Ui_MainWindow):
         space_width = font_metrics.horizontalAdvance(' ')
         self.textEdit.setTabStopDistance(space_width * space_count)
 
-
-    def append_file(self):
-        # (file_path)
-        # file_path, _ = QFileDialog.getOpenFileName(self, "Select Text File to Append", "", "Text Files (*.txt);;All Files (*)")
-        main(file_path)
-        if file_path:
+    def OutputTokens(self):
+        self.TextOutput.clear()
+        output_path = "compiler/Output/tokens_output.txt"
+        if output_path:
             try:
-                with open(file_path, "r", encoding="utf-8") as file:
+                with open(output_path, "r", encoding="utf-8") as file:
                     content = file.read()
                 self.TextOutput.append(content)
             except Exception as e:
                 self.TextOutput.append(f"Failed to append the file.\nError: {e}")
+
+    def OutputFirst(self):
+        First()
+        self.TextOutput3.clear()
+        output_path = "compiler/Output/FIRST_output.txt"
+        if output_path:
+            try:
+                with open(output_path, "r", encoding="utf-8") as file:
+                    content = file.read()
+                self.TextOutput3.append(content)
+            except Exception as e:
+                self.TextOutput3.append(f"Failed to append the file.\nError: {e}")
+
+    def OutputSymboleTable(self):
+        self.tableWidget.clear()
+        output_path = 'compiler/Output/symbol_table.csv'
+        if output_path:
+            try:
+                with open(output_path, "r", encoding="utf-8") as file:
+                    reader = csv.reader(file)
+                    data = list(reader)
+                if data:
+                    self.tableWidget.setRowCount(len(data))
+                    self.tableWidget.setColumnCount(len(data[0]))
+                    for row_idx, row in enumerate(data):
+                        for col_idx, cell in enumerate(row):
+                            self.tableWidget.setItem(row_idx, col_idx, QTableWidgetItem(cell))
+            except Exception as e:
+                print(f"Error reading file: {e}")
 
 
 if __name__ == "__main__":
